5a849b86e1dd3430ac8cd0e6107aacf0092cede6
shinriyo <shinriyo@shinriyos-MacBook.local>
2016-05-06 13:51:28 +0900

    fix chapter6

--- src/chap6/BasicEnv.java
+++ src/chap6/BasicEnv.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
  */
 public class BasicEnv implements Environment {
     protected HashMap<String,Object> values;
-    public BasicEnv() {}
+    public BasicEnv() { values = new HashMap<String, Object>(); }
     public void put(String name, Object value) { values.put(name, value); }
     public Object get(String name) { return values.get(name); }
 }
